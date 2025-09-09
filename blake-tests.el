fbc0a49969f4ec22ec600dd2bb5aac6b298eb07a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2025-09-09 22:58:24 +0200

    test(crypto): extend one round into twelve round check

--- blake-tests.el
+++ blake-tests.el
@@ -284,13 +284,13 @@
                     #x2318A24E2140FC64]
                    state))))
 
-(ert-deftest blake-two-one-round-mix ()
+(ert-deftest blake-two-twelve-round-mix ()
   (let* ((kind blake-two-big)
          (state (vconcat (alist-get kind blake-two-iv)))
-         (schedule (aref (vconcat (alist-get kind blake-two-schedule)) 0))
+         (schedules (vconcat (alist-get kind blake-two-schedule)))
          (msg [#x0000000000636261 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))
     ;; initialize state
-    ;; note: this should be set by the compress func
+    ;; note: this should be set by the blake2 func
     (aset state 0 (blake-two-init-state-zero
                    kind state 0 (alist-get kind blake-two-bits-in-word)))
 
@@ -312,16 +312,136 @@
 
     ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=0, v[16][0]
     (should (= #x6A09E667F2BDC948 (aref state 0)))
-    (should (= 0 (aref msg (aref schedule 1))))
-    (should (= (aref msg 0) (aref msg (aref schedule 0))))
+    (should (= 0 (aref msg (aref (aref schedules 0) 1))))
+    (should (= (aref msg 0) (aref msg (aref (aref schedules 0) 0))))
 
-    (setq state (blake-two-round kind state msg schedule))
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=0, v[16]
+    (should (equal [#x6A09E667F2BDC948 #xBB67AE8584CAA73B #x3C6EF372FE94F82B
+                    #xA54FF53A5F1D36F1 #x510E527FADE682D1 #x9B05688C2B3E6C1F
+                    #x1F83D9ABFB41BD6B #x5BE0CD19137E2179 #x6A09E667F3BCC908
+                    #xBB67AE8584CAA73B #x3C6EF372FE94F82B #xA54FF53A5F1D36F1
+                    #x510E527FADE682D2 #x9B05688C2B3E6C1F #xE07C265404BE4294
+                    #x5BE0CD19137E2179]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=1, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 0)))
     (should (equal [#x86B7C1568029BB79 #xC12CBCC809FF59F3 #xC6A5214CC0EACA8E
                     #x0C87CD524C14CC5D #x44EE6039BD86A9F7 #xA447C850AA694A7E
                     #xDE080F1BB1C0F84B #x595CB8A9A1ACA66C #xBEC3AE837EAC4887
                     #x6267FC79DF9D6AD1 #xFA87B01273FA6DBE #x521A715C63E08D8A
                     #xE02D0975B8D37A83 #x1C7B754F08B7D193 #x8F885A76B6E578FE
                     #x2318A24E2140FC64]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=2, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 1)))
+    (should (equal [#x53281E83806010F2 #x3594B403F81B4393 #x8CD63C7462DE0DFF
+                    #x85F693F3DA53F974 #xBAABDBB2F386D9AE #xCA5425AEC65A10A8
+                    #xC6A22E2FF0F7AA48 #xC6A56A51CB89C595 #x224E6A3369224F96
+                    #x500E125E58A92923 #xE9E4AD0D0E1A0D48 #x85DF9DC143C59A74
+                    #x92A3AAAA6D952B7F #xC5FDF71090FAE853 #x2A8A40F15A462DD0
+                    #x572D17EFFDD37358]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=3, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 2)))
+    (should (equal [#x60ED96AA7AD41725 #xE46A743C71800B9D #x1A04B543A01F156B
+                    #xA2F8716E775C4877 #xDA0A61BCDE4267EA #xB1DD230754D7BDEE
+                    #x25A1422779E06D14 #xE6823AE4C3FF58A5 #xA1677E19F37FD5DA
+                    #x22BDCE6976B08C51 #xF1DE8696BEC11BF1 #xA0EBD586A4A1D2C8
+                    #xC804EBAB11C99FA9 #x8E0CEC959C715793 #x7C45557FAE0D4D89
+                    #x716343F52FDD265E]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=4, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 3)))
+    (should (equal [#xBB2A77D3A8382351 #x45EB47971F23B103 #x98BE297F6E45C684
+                    #xA36077DEE3370B89 #x8A03C4CB7E97590A #x24192E49EBF54EA0
+                    #x4F82C9401CB32D7A #x8CCD013726420DC4 #xA9C9A8F17B1FC614
+                    #x55908187977514A0 #x5B44273E66B19D27 #xB6D5C9FCA2579327
+                    #x086092CFB858437E #x5C4BE2156DBEECF9 #x2EFEDE99ED4EFF16
+                    #x3E7B5F234CD1F804]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=5, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 4)))
+    (should (equal [#xC79C15B3D423B099 #x2DA2224E8DA97556 #x77D2B26DF1C45C55
+                    #x8934EB09A3456052 #x0F6D9EEED157DA2A #x6FE66467AF88C0A9
+                    #x4EB0B76284C7AAFB #x299C8E725D954697 #xB2240B59E6D567D3
+                    #x2643C2370E49EBFD #x79E02EEF20CDB1AE #x64B3EED7BB602F39
+                    #xB97D2D439E4DF63D #xC718E755294C9111 #x1F0893F2772BB373
+                    #x1205EA4A7859807D]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=6, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 5)))
+    (should (equal [#xE58F97D6385BAEE4 #x7640AA9764DA137A #xDEB4C7C23EFE287E
+                    #x70F6F41C8783C9F6 #x7127CD48C76A7708 #x9E472AF0BE3DB3F6
+                    #x0F244C62DDF71788 #x219828AA83880842 #x41CCA9073C8C4D0D
+                    #x5C7912BC10DF3B4B #xA2C3ABBD37510EE2 #xCB5668CC2A9F7859
+                    #x8733794F07AC1500 #xC67A6BE42335AA6F #xACB22B28681E4C82
+                    #xDB2161604CBC9828]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=7, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 6)))
+    (should (equal [#x6E2D286EEADEDC81 #xBCF02C0787E86358 #x57D56A56DD015EDF
+                    #x55D899D40A5D0D0A #x819415B56220C459 #xB63C479A6A769F02
+                    #x258E55E0EC1F362A #x3A3B4EC60E19DFDC #x04D769B3FCB048DB
+                    #xB78A9A33E9BFF4DD #x5777272AE1E930C0 #x5A387849E578DBF6
+                    #x92AAC307CF2C0AFC #x30AACCC4F06DAFAA #x483893CC094F8863
+                    #xE03C6CC89C26BF92]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=8, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 7)))
+    (should (equal [#xFFC83ECE76024D01 #x1BE7BFFB8C5CC5F9 #xA35A18CBAC4C65B7
+                    #xB7C2C7E6D88C285F #x81937DA314A50838 #xE1179523A2541963
+                    #x3A1FAD7106232B8F #x1C7EDE92AB8B9C46 #xA3C2D35E4F685C10
+                    #xA53D3F73AA619624 #x30BBCC0285A22F65 #xBCEFBB6A81539E5D
+                    #x3841DEF6F4C9848A #x98662C85FBA726D4 #x7762439BD5A851BD
+                    #xB0B9F0D443D1A889]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=9, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 8)))
+    (should (equal [#x753A70A1E8FAEADD #x6B0D43CA2C25D629 #xF8343BA8B94F8C0B
+                    #xBC7D062B0DB5CF35 #x58540EE1B1AEBC47 #x63C5B9B80D294CB9
+                    #x490870ECAD27DEBD #xB2A90DDF667287FE #x316CC9EBEEFAD8FC
+                    #x4A466BCD021526A4 #x5DA7F7638CEC5669 #xD9C8826727D306FC
+                    #x88ED6C4F3BD7A537 #x19AE688DDF67F026 #x4D8707AAB40F7E6D
+                    #xFD3F572687FEA4F1]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=10, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 9)))
+    (should (equal [#xE630C747CCD59C4F #xBC713D41127571CA #x46DB183025025078
+                    #x6727E81260610140 #x2D04185EAC2A8CBA #x5F311B88904056EC
+                    #x40BD313009201AAB #x0099D4F82A2A1EAB #x6DD4FBC1DE60165D
+                    #xB3B0B51DE3C86270 #x900AEE2F233B08E5 #xA07199D87AD058D8
+                    #x2C6B25593D717852 #x37E8CA471BEAA5F8 #x2CFC1BAC10EF4457
+                    #x01369EC18746E775]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=11, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 10)))
+    (should (equal [#xE801F73B9768C760 #x35C6D22320BE511D #x306F27584F65495E
+                    #xB51776ADF569A77B #xF4F1BE86690B3C34 #x3CC88735D1475E4B
+                    #x5DAC67921FF76949 #x1CDB9D31AD70CC4E #x35BA354A9C7DF448
+                    #x4929CBE45679D73E #x733D1A17248F39DB #x92D57B736F5F170A
+                    #x61B5C0A41D491399 #xB5C333457E12844A #xBD696BE010D0D889
+                    #x02231E1A917FE0BD]
+                   state))
+
+    ;; note: https://www.rfc-editor.org/rfc/rfc7693#appendix-A, i=12, v[16]
+    (setq state (blake-two-round kind state msg (aref schedules 11)))
+    (should (equal [#x12EF8A641EC4F6D6 #xBCED5DE977C9FAF5 #x733CA476C5148639
+                    #x97DF596B0610F6FC #xF42C16519AD5AFA7 #xAA5AC1888E10467E
+                    #x217D930AA51787F3 #x906A6FF19E573942 #x75AB709BD3DCBF24
+                    #xEE7CE1F345947AA4 #xF8960D6C2FAF5F5E #xE332538A36B6D246
+                    #x885BEF040EF6AA0B #xA4939A417BFB78A3 #x646CBB7AF6DCE980
+                    #xE813A23C60AF3B82]
                    state))))
 
 (provide 'blake-tests)
